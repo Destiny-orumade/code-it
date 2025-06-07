a3db817e05e3aeecca91beb34f910712b95cdcff
Destiny-orumade <destinyorumade@gmail.com>
2025-06-07 12:36:04 +0100

    adj size image

--- my-port/app/components/Siderbar.tsx
+++ my-port/app/components/Siderbar.tsx
@@ -19,7 +19,7 @@ export default function Sidebar() {
   return (
     <aside className="w-full md:w-64 h-auto md:h-screen bg-[#0f172a] text-white p-4 flex md:flex-col flex-row md:items-center items-start justify-between md:justify-start">
       <div className="flex md:flex-col flex-row items-center md:items-center w-full">
-        <img src="/onw.jpeg" alt="Destiny Orumade" className="w-24 h-14 md:w-24  md:h-24 rounded-full" />
+        <img src="/onw.jpeg" alt="Destiny Orumade" className="w-20 h-14 md:w-24  md:h-24 rounded-full" />
         <h2 className="mt-0 md:mt-4 text-lg md:text-xl font-bold md:text-center ml-4 md:ml-0">Destiny Orumade</h2>
         <div className="flex gap-3 md:my-4 ml-auto md:ml-0">
           <a href="https://wa.me/2349044224048" target="_blank"><FaWhatsapp /></a>
